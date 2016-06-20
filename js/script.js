01480d185f38791cda8fbf18e7ee3bda41977aa3
Misacorp <misa.jokisalo@gmail.com>
2016-06-20 18:08:24 +0300

    Paikka content changes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 var text_kaisa = "Kaisa on Jasmiinan pitkäaikainen kämppis ja vielä pidempiaikainen ystävä."
 
- $(document).ready(function(){
+$(document).ready(function() {
       $('.parallax').parallax();
-      $('#jarkanmaili').text(rakennaPosti("kiihamaki","jarka.","l.com","gmai"))
-      $('#kaisanmaili').text(rakennaPosti("kuismanen","kaisak","il.com","gma"))
-    });
+});
 
- $(document).ready(function(){
-    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-    $('.modal-trigger').leanModal();
-  });
+ $(document).one("scroll", function() {
+      $('#kaisanmaili').text(rakennaPosti("kuismanen","kaisak","il.com","gma"));
+      $('#jarkanmaili').text(rakennaPosti("kiihamaki","jarka.","l.com","gmai"));
+      $('#kaisanpuhelin').text(rakennaPuh("99","92","320 ","050 "));
+      $('#jarkanpuhelin').text(rakennaPuh("97","39","847 ","040 "));
+    });
 
 $("#infolista").click(function() {
 	$("#click-here-container").hide();
@@ -18,6 +18,11 @@ $("#infolista").click(function() {
 var rakennaPosti = function(username2, username1, domain2, domain1) {
   var posti = username1+username2+"@"+domain1+domain2;
   return posti;
+}
+
+var rakennaPuh = function(osa4, osa3, osa2, osa1) {
+  var puh = osa1 + osa2 + osa3 + osa4;
+  return puh;
 }
 
 $("#kaisa-button").click(function() {
